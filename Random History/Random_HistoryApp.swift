2be76fd6cbde0010f8f2ff34a4e1599ea024ae6c
bencomo911 <bencomo911@gmail.com>
2023-06-05 14:02:25 -0700

    Initial Commit

---
+++ Random History/Random_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_HistoryApp.swift
+//  Random History
+//
+//  Created by Dafne Bencomo on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Random_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
