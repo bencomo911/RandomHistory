182e6d8081ab9b42faade737c27a69ff0ffb4f0b
Dafne Bencomo <bencomo911@gmail.com>
2023-06-06 17:54:47 -0700

    First draft for Question screen!

--- Random History/ContentView.swift
+++ Random History/ContentView.swift
@@ -8,14 +8,90 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // Initialize color constants
+    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+    
+    // body starts here
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, World!!!")
-        }
-        .padding()
+        ZStack{
+            // Add background color
+            mainColor.ignoresSafeArea()
+            
+            /* declare VStack to organize current question number, question and buttons
+             */
+            VStack{
+                
+                // Current question counter
+                Text("1 / 10")
+                    .font(.callout)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                
+                // Question
+                Text("What was the first computer bug?")
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                Spacer()
+                
+                /*Buttons
+                  declare HStack to add buttons
+                 */
+                HStack{
+                    
+                    // Button 1
+                    Button(action: {
+                        print("Tapped on Choice 1")
+                    }, label: {
+                        Text("Ant")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    // Button 2
+                    Button(action: {
+                        print("Tapped on Choice 2")
+                    }, label: {
+                        Text("Beetle")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    //Button 3
+                    Button(action: {
+                        print("Tapped on Choice 3")
+                    }, label: {
+                        Text("Moth")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    //Button 4
+                    Button(action: {
+                        print("Tapped on Choice 4")
+                    }, label: {
+                        Text("Fly")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    
+                }// end of HStack
+                
+            }// end of VStack
+            
+        } // end of ZStack
+        .foregroundColor(.white)
     }
 }
 
