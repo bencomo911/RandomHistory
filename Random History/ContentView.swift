dd9b33ad66bfa875699ea4f00cd690e5f759a228
Dafne Bencomo <bencomo911@gmail.com>
2023-07-08 12:47:39 -0700

    Added Question Model file and ChoiceTextView view and refactored most of ContentView code

--- Random History/ContentView.swift
+++ Random History/ContentView.swift
@@ -8,10 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Question instance
+    let question = Question(
+        questionText: "What was the first computer BUG?",
+        possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"],
+        correctAnswerIndex: 2
+    )
     
     // Initialize color constants
-    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
-    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+    @State var mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
     
     // body starts here
     var body: some View {
@@ -30,7 +35,7 @@ struct ContentView: View {
                     .padding()
                 
                 // Question
-                Text("What was the first computer bug?")
+                Text(question.questionText)
                     .font(.largeTitle)
                     .bold()
                     .multilineTextAlignment(.leading)
@@ -40,52 +45,15 @@ struct ContentView: View {
                   declare HStack to add buttons
                  */
                 HStack{
-                    
-                    // Button 1
-                    Button(action: {
-                        print("Tapped on Choice 1")
-                    }, label: {
-                        Text("Ant")
-                            .font(.body)
-                            .bold()
-                            .multilineTextAlignment(.center)
-                            .padding()
-                            .border(accentColor, width: 4)
-                    })
-                    // Button 2
-                    Button(action: {
-                        print("Tapped on Choice 2")
-                    }, label: {
-                        Text("Beetle")
-                            .font(.body)
-                            .bold()
-                            .multilineTextAlignment(.center)
-                            .padding()
-                            .border(accentColor, width: 4)
-                    })
-                    //Button 3
-                    Button(action: {
-                        print("Tapped on Choice 3")
-                    }, label: {
-                        Text("Moth")
-                            .font(.body)
-                            .bold()
-                            .multilineTextAlignment(.center)
-                            .padding()
-                            .border(accentColor, width: 4)
-                    })
-                    //Button 4
-                    Button(action: {
-                        print("Tapped on Choice 4")
-                    }, label: {
-                        Text("Fly")
-                            .font(.body)
-                            .bold()
-                            .multilineTextAlignment(.center)
-                            .padding()
-                            .border(accentColor, width: 4)
-                    })
-                    
+                    ForEach(0..<question.possibleAnswers.count){ answerIndex in
+                        Button(action: {
+                            print("Tapped on button with the text: \(question.possibleAnswers[answerIndex])")
+                            mainColor = answerIndex == question.correctAnswerIndex ? .green : .red
+                            }, label: {
+                                ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
+                            }
+                        ) //end of Button
+                    }// end of ForEach
                 }// end of HStack
                 
             }// end of VStack
---
+++ Random History/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  Random History
+//
+//  Created by Dafne Bencomo on 7/8/23.
+//
+
+struct Question{
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+    
+}
